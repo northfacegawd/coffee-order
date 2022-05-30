3835b1e68cab739bfbbb32580e2ca6a936fcdd8c
Lee yongjun <chuck5732590@gmail.com>
2022-05-30 21:20:04 +0900

    header 레이아웃 수정

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -55,15 +55,38 @@ export default function Header() {
           </svg>
         </button>
         {/* DESKTOP VERSION */}
-        <ul className="hidden lg:block">
-          <li>
-            <Link href="/menu">
-              <a className="flex items-center justify-center h-full font-medium">
-                메뉴
-              </a>
-            </Link>
-          </li>
-        </ul>
+        <div className="w-full hidden lg:flex justify-between items-center">
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/menu">
+                <a className="flex items-center justify-center h-full font-medium hover:text-orange-500 transition-colors">
+                  메뉴
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/menu">
+                <a className="flex items-center justify-center h-full font-medium hover:text-orange-500 transition-colors">
+                  메뉴
+                </a>
+              </Link>
+            </li>
+          </ul>
+          <button className="text-orange-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
       </nav>
       {/* back drop */}
       <CSSTransition
