36cfe4e6e64bb1340bab48c1ff70f8526cff4ba8
Lee yongjun <chuck5732590@gmail.com>
2022-06-18 17:36:16 +0900

    feat(settings): prettier, eslint 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import "../styles/globals.scss";
-import Layout from "@components/layout";
-import type { AppProps } from "next/app";
+import React from 'react';
+import type { AppProps } from 'next/app';
+
+import '@styles/globals.scss';
+import Layout from '@components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from "next";
+import React from 'react';
+import type { NextPage } from 'next';
 
-import homeStyles from "./index.module.scss";
+import homeStyles from './index.module.scss';
 
-const Home: NextPage = () => {
+const Home: NextPage = function Home() {
   return (
     <section className={homeStyles.grid}>
       {/* 메인 카테고리 부분 */}
@@ -22,40 +23,64 @@ const Home: NextPage = () => {
       <section className="flex flex-col">
         {/* 서브 카테고리 부분 */}
         <nav className="flex space-x-4 border-2 border-orange-500 rounded-lg mb-4 font-medium w-full flex-wrap">
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             콜드브루
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             라떼
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             에스프레소
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             디카페인
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             콜드브루
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             라떼
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             에스프레소
           </button>
-          <button className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium">
+          <button
+            type="button"
+            className="p-2 rounded-md bg-orange-500 text-white shadow-md focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none transition-colors m-2 text-sm font-medium"
+          >
             디카페인
           </button>
         </nav>
         {/* 메뉴 */}
         <div className="bg-slate-100 mx-auto">
           <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
-            {[...Array(40)].map((_, index) => (
+            {[...Array(40)].map((_) => (
               <li
-                key={index}
+                key={_}
                 className="bg-slate-300 w-40 h-40 md:w-44 md:h-44 lg:w-48 lg:h-48"
               >
-                <img className="w-full h-auto" />
+                <img className="w-full h-auto" alt="coffee" />
               </li>
             ))}
           </ul>
